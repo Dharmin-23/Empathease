d4ece2634964681adfbb26c0884cbebc3c304e81
Dharmin-23 <dharmin23072002@gmail.com>
2024-05-10 01:26:02 +0530

    Modfied Login

--- src/screens/SignIn/SignIn.js
+++ src/screens/SignIn/SignIn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useEffect } from 'react';
-import { View, Text, TouchableOpacity, Modal } from 'react-native';
+import { View, Text, TouchableOpacity, Modal, ImageBackground } from 'react-native'; // Import ImageBackground
 import { Center, Box, VStack, FormControl, Heading, Input, Button, HStack, Link, Image } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
@@ -10,13 +10,15 @@ import { baseUrl } from '../../constants/Constants';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ScrollView } from 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native';
-
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const SignIn = () => {
-  // var res = 1;
   const navigation = useNavigation();
   const [isUserNotExistPopupVisible, setUserNotExistPopupVisible] = useState(false);
+  const [showPassword, setShowPassword] = useState(false); 
+  const toggleShowPassword = () => { 
+    setShowPassword(!showPassword); 
+  }
 
   useEffect(() => {
     const checkLoginStatus = async () => {
@@ -35,13 +37,12 @@ const SignIn = () => {
 
     checkLoginStatus();
   }, []);
-  const [isOtpVisible, setIsOtpVisible] = useState(false);
 
   const [user, setUser] = useState({
     username: '',
     password: '',
   });
-
+  
   const handleChange = (name, value) => {
     setUser({
       ...user,
@@ -50,26 +51,18 @@ const SignIn = () => {
   };
 
   const handleSignIn = async () => {
-    console.log(user)
-    
-
-    
     try {
       res = await axios.post(baseUrl + "/auth/login", user);
 
-      
       if (res.status === 200) {
-        console.log("login nresponse"+res.data)
         const token = res.data.payload.token;
         const username = res.data.payload.username;
         const userId = res.data.payload.id;
-        // console.log(res.data.payload )
-      
         
         AsyncStorage.setItem("username", username)
         AsyncStorage.setItem("authToken", token);
         AsyncStorage.setItem("userId", String(userId))
-        console.log("Useername at time of login: "+username+"UsewrId: "+userId)
+        
         navigation.navigate('AnimTab');
         setIsOtpVisible(true); // Show OTP verification modal
       }
@@ -111,69 +104,86 @@ const SignIn = () => {
   );
 
   return (
-    <ScrollView>
-    <Center w="100%">
-      <Box safeArea p="2" py="0" w="80%" maxW="290">
-        <Image
-          source={require('../../assets/images/bottomart.png')}
-          style={{ width: '100%', resizeMode: 'contain' }}
-          alt='Bottom bc'
-        />
-        <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{ color: "warmGray.50" }}>
-          Welcome
-        </Heading>
-        <Heading mt="1" _dark={{ color: "warmGray.200" }} color="coolGray.600" fontWeight="medium" size="xs">
-          Sign in to continue!
-        </Heading>
-        {/* <UserNotExistPopup/> */}
+    <ImageBackground source={require('../../assets/images/login-wallpaper.png')} style={styles.backgroundImage}>
+      <ScrollView>
+        <Center w="100%">
+          <Box safeArea p="2" py="0" w="80%" maxW="290">
+            <Image
+              source={require('../../assets/images/logo.png')}
+              style={{ width: '100%', resizeMode: 'contain' }}
+              alt='Bottom bc'
+            />
+            <Heading size="lg" fontWeight="600" color="coolGray.800" mt="-10" _dark={{ color: "warmGray.50" }}>
+              Welcome
+            </Heading>
+            <Heading mt="1" _dark={{ color: "warmGray.200" }} color="coolGray.600" fontWeight="medium" size="xs">
+              Sign in to continue!
+            </Heading>
 
-        <VStack space={3} mt="5">
-          <FormControl>
-            <FormControl.Label>Enter your Username</FormControl.Label>
-            <Input 
-            value={user.username} 
-            onChangeText={(value) => handleChange('username', value)} 
-            color="white"/>
-          </FormControl>
-          <FormControl>
-            <FormControl.Label>Password</FormControl.Label>
-            <Input 
-            value={user.password} 
-            onChangeText={(value) => handleChange('password', value)} 
-            type="password" 
-            color="white"/>
-            <Link _text={{ fontSize: "xs", fontWeight: "500", color: "indigo.500" }} alignSelf="flex-end" mt="1">
-              Forget Password?
-            </Link>
-          </FormControl>
-          <Button mt="2" colorScheme="indigo" onPress={handleSignIn}>
-            Sign in
-          </Button>
-          {/* <OtpVerification isVisible={isOtpVisible} onClose={handleOtpVerificationClose} /> */}
-          <HStack mt="6" justifyContent="center">
-            <Text fontSize="sm" color="coolGray.600" _dark={{ color: "warmGray.200" }}>
-              I'm a new user.{" "}
-            </Text>
-            <TouchableOpacity onPress={handleSignUp}>
-              <Text style={{ color: "indigo" }}>Don't have an account? Register now!</Text>
-            </TouchableOpacity>
-          </HStack>
-        </VStack>
-      </Box>
+            <VStack space={3} mt="5">
+              <FormControl>
+                <FormControl.Label>Enter your Username</FormControl.Label>
+                <Input 
+                  value={user.username} 
+                  onChangeText={(value) => handleChange('username', value)} 
+                  color="black"
+                />
+              </FormControl>
+              <FormControl>
+                <FormControl.Label>Password</FormControl.Label>
+                <View style={styles.passwordInputContainer}>
+                  <Input 
+                    secureTextEntry={!showPassword}
+                    value={user.password} 
+                    onChangeText={(value) => handleChange('password', value)} 
+                    type="password" 
+                    color="black"
+                  />
+                  <MaterialCommunityIcons 
+                    name={showPassword ? 'eye-off' : 'eye'} 
+                    size={24} 
+                    color="#aaa"
+                    style={styles.icon} 
+                    onPress={toggleShowPassword} 
+                  /> 
+                </View>
+                <Link _text={{ fontSize: "xs", fontWeight: "500", color: "indigo.500" }} alignSelf="flex-end" mt="1">
+                  Forget Password?
+                </Link>
+              </FormControl>
+              <Button mt="2" colorScheme="indigo" onPress={handleSignIn}>
+                Sign in
+              </Button>
+              <HStack mt="6" justifyContent="center">
+                {/* <Text fontSize="sm" color="coolGray.600" _dark={{ color: "black.500" }}>
+                  I'm a new user.{" "}
+                </Text> */}
+                <TouchableOpacity onPress={handleSignUp}>
+                  <Text style={{ color: "indigo" }}>Don't have an account? Register now!</Text>
+                </TouchableOpacity>
+              </HStack>
+            </VStack>
+          </Box>
 
-      <View>
-        <Image
-          source={require('../../assets/images/bottomart.png')}
-          style={{ width: '100%', height: 100, resizeMode: 'contain' }}
-          alt='le bhai bas'
-        />
-      </View>
-    </Center>
-    </ScrollView>
+          <View>
+            <Image
+              source={require('../../assets/images/bottomart.png')}
+              style={{ width: '100%', height: 100, resizeMode: 'contain' }}
+              alt='le bhai bas'
+            />
+          </View>
+        </Center>
+      </ScrollView>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
   popupContainer: {
     flex: 1,
     justifyContent: 'center',
@@ -190,6 +200,16 @@ const styles = StyleSheet.create({
     marginTop: 10,
     color: 'blue',
     textDecorationLine: 'underline',
+  },
+  passwordInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1, // Assuming you want a border under the password field
+    borderBottomColor: 'gray', // You can adjust the color as needed
+  },
+  icon: {
+    position: 'absolute',
+    right: 10, // Adjust this value as needed to position the icon properly
   },
 });
 
